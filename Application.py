9b93619a1f1175887d51ee64fe0ca1117f71775e
strotman.7 <ryan.strotman@gmail.com>
2019-11-11 20:25:48 -0500

    changed to debug = true

--- Application.py
+++ Application.py
@@ -1,8 +1,5 @@
 #Flask application for megan
 
-
-
-
 from flask import Flask
 
 application = app = Flask(__name__)
@@ -13,5 +10,5 @@ def index():
     return "<h1> Hello World for the second time </h1>"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
 
