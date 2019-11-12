32baaadc859bc39b20668296117c83769b86e997
strotman.7 <ryan.strotman@gmail.com>
2019-11-11 22:29:48 -0500

    added redirects

--- Application.py
+++ Application.py
@@ -1,31 +1,38 @@
 #Flask application for megan
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 application = app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY']  = "j;LJD:OSIFjFDFP(*Y(PSPA*Y))"
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+print('sqlite:///' + os.path.join(basedir, 'DataBase.sqlite'))
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "hello world" # render_template('index.html')
 
 @app.route('/customers/add', methods=['GET','POST'])
 def addcustomer():
-    address = None
-    last_name = None
     form = AddCustomer()
     if form.validate_on_submit():
-        address = form.address.data
-        last_name = form.last_name.data
+        session["address"] = form.address.data
+        flash("Good adding")
+        session["last_name"] = form.last_name.data
         form.address.data = None
         form.last_name.data = None
-    return render_template('addcustomer.html',form = form, address = address, name = last_name)
+        return redirect(url_for('addcustomer'))
+    return render_template('addcustomer.html',form = form, address = session.get("address"), name = session.get("last_name"))
 
 
 class AddCustomer(FlaskForm):
