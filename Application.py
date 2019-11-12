f81a320b3c863bbdbc9d9ae37f92e1c787119bbf
strotman.7 <ryan.strotman@gmail.com>
2019-11-11 20:18:32 -0500

    made requirements.txt

--- Application.py
+++ Application.py
@@ -5,12 +5,12 @@
 
 from flask import Flask
 
-app = Flask(__name__)
+application = app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "<h1> Hello World </h1>"
+    return "<h1> Hello World for the second time </h1>"
 
 if __name__ == '__main__':
     app.run()
